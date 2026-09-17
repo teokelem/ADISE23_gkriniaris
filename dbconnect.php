1ab74f5f08257d373ceebe886762d3738f10ba85
teokelem <148782754+teokelem@users.noreply.github.com>
2026-09-17 10:29:36 +0300

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+$host='localhost';
+$db = 'adise23_griniaris';
+require_once "db_upass.php";
+
+$user=$DB_USER;
+$pass=$DB_PASS;
+
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/iee/2019/iee2019068/mysql/run/mysql.sock');
+} else {
+		$pass=null;
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}?>
